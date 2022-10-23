b934e75b493d1015b62e1ab35a50a4894314b177
Anna Melnichuk <varchuk.anna.94@gmail.com>
2022-10-23 22:11:07 +0300

    test

--- src/components/Services-card/Services-card.jsx
+++ src/components/Services-card/Services-card.jsx
@@ -10,6 +10,10 @@ const ServicesCard = ({imgKey, title, price}) => {
     const payment = document.getElementById('payment');
     payment.dataset.amount = buttonValue;
 
+    var DOMContentLoaded_event = document.createEvent("Event")
+    DOMContentLoaded_event.initEvent("DOMContentLoaded", true, true)
+    window.document.dispatchEvent(DOMContentLoaded_event);
+
 
     const setPrice = (e) => {
         const priceValue = parseInt(e.target.dataset.price);
@@ -22,11 +26,11 @@ const ServicesCard = ({imgKey, title, price}) => {
             <li className={styles.card}>
                 <img className={styles.card_img} src={getImageByKey(imgKey)} alt={title} />
                 <div className={styles.card_text_wrapper}>
-                    <div className={styles.price_container}>
+                    <div onClickCapture={setPrice}  className={styles.price_container}>
                         <h3 className={styles.card_title}>{title}</h3>
                         <div className={styles.price_btn_container}>
-                            <button  className='rsWidgetButton' >Buy</button>
-                            {/* <Button data-price={price} variant="warning"  className='rsWidgetButton'>Price {price / 100}$</Button> */}
+                            {/* <button  className='rsWidgetButton' >Buy</button> */}
+                            <Button data-price={price} variant="warning"  className='rsWidgetButton'>Price {price / 100}$</Button>
                             </div>
                         </div>
                     <SeeMoreBtn />
