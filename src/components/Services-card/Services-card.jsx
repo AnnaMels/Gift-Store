d8a587ae7eec5bfb5d396e7ab23ecd3cd64ab6d9
Anna Melnichuk <varchuk.anna.94@gmail.com>
2022-10-23 21:24:10 +0300

    Test

--- src/components/Services-card/Services-card.jsx
+++ src/components/Services-card/Services-card.jsx
@@ -4,8 +4,9 @@ import SeeMoreBtn from '../SeeMoreBtn';
 import { Button } from 'react-bootstrap';
 import getImageByKey from "../../assets/getImageByKey";
 
+import ReactTouchEvents from "react-touch-events";
 
-const ServicesCard = ({imgKey, title, description, price}) => {
+const ServicesCard = ({imgKey, title, price}) => {
     const [buttonValue, setButtonValue] = useState(0);
     const payment = document.getElementById('payment');
     payment.dataset.amount = buttonValue;
@@ -21,12 +22,15 @@ const ServicesCard = ({imgKey, title, description, price}) => {
             <li className={styles.card}>
                 <img className={styles.card_img} src={getImageByKey(imgKey)} alt={title} />
                 <div className={styles.card_text_wrapper}>
-                    <div  className={styles.price_container}>
+                    <ReactTouchEvents onTap={setPrice}>
+                    <div onClickCapture={setPrice} onTouchMoveCapture={setPrice} className={styles.price_container}>
                         <h3 className={styles.card_title}>{title}</h3>
                         <div className={styles.price_btn_container}>
-                            <Button  onTouchStart={setPrice} data-price={price} variant="warning" id="mainButton" className='rsWidgetButton'>Price {price / 100}$</Button>
+                            <Button data-price={price} variant="warning" id="mainButton" className='rsWidgetButton'>Price {price / 100}$</Button>
+                            </div>
+                            
                         </div>
-                    </div>
+                        </ReactTouchEvents>
                     <SeeMoreBtn />
                 </div>
             </li>
